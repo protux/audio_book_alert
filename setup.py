116187440c9382cf6aa944f0e1e9995c884acdd8
Nico Schwanebeck <gitlab@nischwan.de>
2021-03-09 19:52:04 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+setup(
+    name='Audio Book Alert',
+    version='0.9',
+    description='A script for getting alerted about new audio books on Audible via Telegram',
+    author='protux',
+    package_dir={'audio_book_alert': 'audio_book_alert'},
+    packages=find_packages()
+)
