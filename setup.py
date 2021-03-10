81e3600f78e46815a89b44fc46968bd5c0caa37a
Nico Schwanebeck <gitlab@nischwan.de>
2021-03-10 23:21:33 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 
 setup(
     name='Audio Book Alert',
-    version='0.9',
+    version='0.10',
     description='A script for getting alerted about new audio books on Audible via Telegram',
     author='protux',
     package_dir={'audio_book_alert': 'audio_book_alert'},
