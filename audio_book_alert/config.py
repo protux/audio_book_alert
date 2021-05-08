a90d2ca11f7772904027134e984c9a6994518925
Schwanebeck, Nico <Nico.Schwanebeck@adesso.de>
2021-05-08 21:54:59 +0200

    update requirements; add feature flag for db

--- audio_book_alert/config.py
+++ audio_book_alert/config.py
@@ -7,3 +7,5 @@ from pydantic import (
 class Settings(BaseSettings):
     telegram_bot_name: str = Field(..., env='ABA_TELEGRAM_BOT_NAME')
     telegram_api_key: str = Field(..., env='ABA_TELEGRAM_API_KEY')
+
+    activate_database: bool = Field(..., env='ABA_ACTIVATE_DATABASE', default=False)
