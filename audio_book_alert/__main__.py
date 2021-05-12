b93a3cf7eaec9b40d7719b6b9effc7f343dec4d8
Schwanebeck, Nico <Nico.Schwanebeck@adesso.de>
2021-05-12 14:37:44 +0200

    add save to main

--- audio_book_alert/__main__.py
+++ audio_book_alert/__main__.py
@@ -1,16 +1,16 @@
 import sys
 from typing import List
 
-from audio_book_alert.alert.send_alert_service import SendAlertService
 from audio_book_alert.alert import telegram_bot
+from audio_book_alert.alert.send_alert_service import SendAlertService
+from audio_book_alert.database.orm import get_db
 from audio_book_alert.scraper import parser_config
 from audio_book_alert.scraper import (
     scraper,
     filter,
 )
-from audio_book_alert.storage import audio_book_file_repository
 from audio_book_alert.storage.audio_book import AudioBook
-from audio_book_alert.database.orm import get_db
+from audio_book_alert.storage.audio_book_repository import AudioBookRepository
 
 
 def parse_audio_books() -> None:
@@ -22,8 +22,12 @@ def parse_audio_books() -> None:
     for narrator in parser_config.narrators:
         audio_books += scraper.find_titles_by_narrator(narrator.strip())
 
-    past_audio_books = audio_book_file_repository.get_all_audio_books()
-    filtered_audio_books = filter.filter_audio_books(audio_books, past_audio_books)
+    audio_book_repository: AudioBookRepository = AudioBookRepository(db_session)
+    past_audio_books: List[AudioBook] = audio_book_repository.get_all_audio_books()
+    filtered_audio_books: List[AudioBook] = filter.filter_audio_books(
+        audio_books, past_audio_books
+    )
+    audio_book_repository.save_audio_books(filtered_audio_books)
     SendAlertService(db_session).send_alert(filtered_audio_books)
 
 
