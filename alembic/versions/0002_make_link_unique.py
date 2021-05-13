1996a99de81f0077b967445f0e66c2ab95cfeda5
Schwanebeck, Nico <Nico.Schwanebeck@adesso.de>
2021-05-13 15:46:42 +0200

    make link unique in db

---
+++ alembic/versions/0002_make_link_unique.py
@@ -0,0 +1,22 @@
+"""make link unique
+
+Revision ID: 0002
+Revises: 0001
+Create Date: 2021-05-13 15:45:33.957455
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '0002'
+down_revision = '0001'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(None, 'audio_books', ['link'])
+
+
+def downgrade() -> None:
+    op.drop_constraint(None, 'audio_books', type_='unique')
